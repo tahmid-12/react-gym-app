b2377a9e2f0cdd0fd08e02d771cc0e9c3f781cda
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-09-02 01:03:12 +0600

    Header navbar done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./Header.css";
 import { nav } from '../../data/Data';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
+
+  const [show, setShow] = useState(false);
+
+  const navShow = () => {
+    setShow(!show);
+  }
+
   return (
     <>
       <header>
@@ -10,10 +19,33 @@ const Header = () => {
                 <img src="./images/logo.png" alt="" />
             </div>
             <div className="nav">
-                <ul>
-                    
+                <ul className={show ? "small" : "flex"}>
+                    {
+                      nav.map((list, index) => (
+                        <li key={index}>
+                          <Link to={list.path}>{list.text}</Link>
+                        </li>
+                      ))
+                    }
                 </ul>
             </div>
+            <div className="button flex">
+              <h4>
+                <span>2</span> My List
+              </h4>
+              <button className="btn1">
+                <i className='fa fa-sign-out'></i> SIgn In
+              </button>
+            </div>
+
+            <div className="toggle">
+              <button onClick={navShow}>
+                {
+                  show ? <i className='fa fa-times'></i> : <i className='fa fa-bars'></i>
+                }
+              </button>
+            </div>
+
         </div>
       </header>
     </>
