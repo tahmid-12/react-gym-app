909d84a96e57a85a6bd0fd0b4125b9501a87a8d3
tahmid-12 <mdtahmidchowdhury12@gmail.com>
2024-09-10 09:53:18 +0600

    Programs Component Added

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
+import Programs from "./Programs/Programs";
 
 export default{
     Hero,
-    Header
+    Header,
+    Programs
 };
